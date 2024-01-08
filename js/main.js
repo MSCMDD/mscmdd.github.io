174047efe34f2d89b47829997e3471573616914e
MSCMDD <MSCMDD@users.noreply.github.com>
2024-01-08 03:15:10 +0000

    deploy: bf588c41a07d342864fbe9932e08199ab624b879

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ const util = {
       el.select();
       document.execCommand("Copy");
       if (msg && msg.length > 0) {
-        hud.toast(msg);
+        hud.toast(msg, 2500);
       }
     }
   },
@@ -59,17 +59,15 @@ const util = {
 
 const hud = {
   toast: (msg, duration) => {
-    duration = isNaN(duration) ? 2000 : duration;
+    const d = Number(isNaN(duration) ? 2000 : duration);
     var el = document.createElement('div');
     el.classList.add('toast');
+    el.classList.add('show');
     el.innerHTML = msg;
     document.body.appendChild(el);
-    setTimeout(function () {
-      var d = 0.5;
-      el.style.webkitTransition = '-webkit-transform ' + d + 's ease-in, opacity ' + d + 's ease-in';
-      el.style.opacity = '0';
-      setTimeout(function () { document.body.removeChild(el) }, d * 1000);
-    }, duration);
+
+    setTimeout(function(){ document.body.removeChild(el) }, d);
+    
   },
 
 }
@@ -126,7 +124,7 @@ const init = {
               highlightItem.addClass("active")
               const e0 = document.querySelector('.widgets')
               const e1 = document.querySelector('#data-toc a.toc-link[href="' + encodeURI(link) + '"]')
-              const offsetBottom = e1.getBoundingClientRect().bottom - e0.getBoundingClientRect().bottom + 100
+              const offsetBottom = e1.getBoundingClientRect().bottom - e0.getBoundingClientRect().bottom + 200
               const offsetTop = e1.getBoundingClientRect().top - e0.getBoundingClientRect().top - 64
               if (offsetTop < 0) {
                 e0.scrollBy(0, offsetTop)
@@ -244,9 +242,12 @@ if (stellar.plugins.stellar) {
       const els = document.getElementsByClassName('stellar-' + key + '-api');
       if (els != undefined && els.length > 0) {
         stellar.jQuery(() => {
-          stellar.loadScript(js, { defer: true });
-          if (key == 'timeline') {
-            stellar.loadScript(stellar.plugins.marked);
+          if (key == 'timeline' || 'memos') {
+            stellar.loadScript(stellar.plugins.marked).then(function () {
+              stellar.loadScript(js, { defer: true });
+            });
+          } else {
+            stellar.loadScript(js, { defer: true });
           }
         })
       }
@@ -254,59 +255,30 @@ if (stellar.plugins.stellar) {
   }
 }
 
-// ------------------- start 首页置顶文章轮播  新增
+// swiper
 if (stellar.plugins.swiper) {
- const swiper_container = document.getElementById('swiper_container');
- if (swiper_container !== undefined) {
-   stellar.loadCSS(stellar.plugins.customSwiperTopArticle.css);
-   stellar.loadScript(stellar.plugins.customSwiperTopArticle.js, {defer:true}).then(function () {
-     var swiper = new Swiper('.blog-slider', {
-       passiveListeners: true,
-       spaceBetween: 30,
-       effect: 'fade',
-       loop: true,
-       autoplay: {
-         disableOnInteraction: true,
-         delay: 3000
-       },
-       mousewheel: false,
-       // autoHeight: true,
-       pagination: {
-         el: '.blog-slider__pagination',
-         clickable: true,
-       }
-     });
-     swiper_container.onmouseenter = function() {
-       swiper.autoplay.stop();
-     };
-     swiper_container.onmouseleave = function() {
-       swiper.autoplay.start();
-     }
-   });
- }
-// ------------------- end 首页置顶文章轮播  新增
-
- // swiper
- const swiper_api = document.getElementById('swiper-api');
- if (swiper_api != undefined) {
-   stellar.loadCSS(stellar.plugins.swiper.css);
-   stellar.loadScript(stellar.plugins.swiper.js, {defer:true}).then(function () {
-     var swiper = new Swiper('.swiper-container', {
-       slidesPerView: 'auto',
-       spaceBetween: 8,
-       centeredSlides: true,
-       loop: true,
-       pagination: {
-         el: '.swiper-pagination',
-         clickable: true,
-       },
-       navigation: {
-         nextEl: '.swiper-button-next',
-         prevEl: '.swiper-button-prev',
-       },
-     });
-   })
- }
+  const swiper_api = document.getElementById('swiper-api');
+  if (swiper_api != undefined) {
+    stellar.loadCSS(stellar.plugins.swiper.css);
+    stellar.loadScript(stellar.plugins.swiper.js, { defer: true }).then(function () {
+      const effect = swiper_api.getAttribute('effect') || '';
+      var swiper = new Swiper('.swiper#swiper-api', {
+        slidesPerView: 'auto',
+        spaceBetween: 8,
+        centeredSlides: true,
+        effect: effect,
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+    })
+  }
 }
 
 // preload
@@ -328,13 +300,35 @@ if (stellar.plugins.preload) {
   }
 }
 
+function loadFancybox() {
+  stellar.loadCSS(stellar.plugins.fancybox.css);
+  stellar.loadScript(stellar.plugins.fancybox.js, { defer: true }).then(function () {
+    Fancybox.bind(selector, {
+      groupAll: true,
+      hideScrollbar: false,
+      Thumbs: {
+        autoStart: false,
+      },
+      caption: function (fancybox, carousel, slide) {
+        return slide.$trigger.alt || null
+      }
+    });
+  })
+}
 // fancybox
 if (stellar.plugins.fancybox) {
   let selector = 'img[fancybox]:not(.error)';
   if (stellar.plugins.fancybox.selector) {
     selector += `, ${stellar.plugins.fancybox.selector}`
   }
-  if (document.querySelectorAll(selector).length !== 0) {
+  var needFancybox = document.querySelectorAll(selector).length !== 0;
+  if (!needFancybox) {
+    const els = document.getElementsByClassName('stellar-memos-api');
+    if (els != undefined && els.length > 0) {
+      needFancybox = true;
+    }
+  }
+  if (needFancybox) {
     stellar.loadCSS(stellar.plugins.fancybox.css);
     stellar.loadScript(stellar.plugins.fancybox.js, { defer: true }).then(function () {
       Fancybox.bind(selector, {
@@ -413,4 +407,8 @@ if (stellar.plugins.heti) {
 
     stellar.plugins.heti.enable = false;
   });
+}
+
+if (stellar.plugins.copycode) {
+  stellar.loadScript(stellar.plugins.copycode.js, { defer: true })
 }
